6de973364ccbbacd023052ac2e7c3c640496f079
Santiago Overdinelli <santiagoverdinelli23@gmail.com>
2026-08-20 14:58:06 -0300

    AHORA SIII

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,15 @@ const nextConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "jutfhzedvmnsepmkoagu.supabase.co",
+        pathname: "/storage/v1/object/public/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
